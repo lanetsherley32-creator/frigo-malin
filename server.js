0d9924471f791895415228f74ce42562c6317fab
Sherley <lanetsherley32@gmail.com>
2026-08-21 16:43:22 +0200

    fix: supprimer la double declaration de path dans server.js

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const io = new Server(server);
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const path = require('path');
 const db = new sqlite3.Database(path.resolve(__dirname, 'database.sqlite'));
 
 db.serialize(() => {
